2ef96d59250dbe416cd9fcd85ee98da680f4aff0
krmax44 <hi@krmax44.de>
2019-08-27 21:04:39 +0200

    resolve merge conflict

--- src/layouts/contact.vue
+++ src/layouts/contact.vue
@@ -60,7 +60,7 @@
         <input
           type="submit"
           value="Submit"
-          class="block bg-blue hover:bg-blue-dark text-white text-sm font-semibold tracking-wide uppercase shadow rounded-lg cursor-pointer px-6 py-3"
+          class="block bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold tracking-wide uppercase shadow rounded-lg cursor-pointer px-6 py-3"
         />
       </div>
     </div>
